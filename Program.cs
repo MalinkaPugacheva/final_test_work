7d35973890efb4748a4d473b462e5a5fb0650a4e
Malinka <alina-malakhovich@rambler.ru>
2022-10-07 21:06:57 +0300

    Work is done

--- Program.cs
+++ Program.cs
@@ -8,4 +8,20 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
-git
+Console.Write("How many elements of array do you want? ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+int len = 3;
+int pos = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Write {i+1} element: ");
+    string element = Convert.ToString(Console.ReadLine());
+    if (element.Length <= len)
+    {
+        arrayStrings[pos] = element;
+        pos++;
+    }
+}
+Console.WriteLine();
+PrintArray(arrayStrings);
